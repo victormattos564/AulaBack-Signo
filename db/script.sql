e83a8f39a4248601140a06e2847f1eb53e775e6a
Victor Mattos <123407132+victormattos564@users.noreply.github.com>
2024-04-25 13:59:55 -0300

    Updated table error

--- db/script.sql
+++ db/script.sql
@@ -1,53 +1,53 @@
 CREATE DATABASE aulabackend;
 
-CREATE TABLE usuarios (
+CREATE TABLE usuários (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    sobrenome VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    data_nascimento DATE NOT NULL,
-    idade INTEGER NOT NULL,
+    birthdate DATE NOT NULL,
+    age INTEGER NOT NULL,
     signo_zodiacal VARCHAR(100) NOT NULL
 );
 
-CREATE FUNCTION calcular_idade(data_nascimento DATE) RETURNS INTEGER AS $$
+CREATE FUNCTION calculate_age(birthdate DATE) RETURNS INTEGER AS $$
 DECLARE
-    idade INTEGER;
+    age INTEGER;
 BEGIN
-    idade := EXTRACT(YEAR FROM AGE(NOW(), data_nascimento));
-    RETURN idade;
+    age := EXTRACT(YEAR FROM AGE(NOW(), birthdate));
+    RETURN age;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION calcular_signo_zodiacal(data_nascimento DATE) RETURNS VARCHAR AS $$
+CREATE FUNCTION calculate_signo_zodiacal(birthdate DATE) RETURNS VARCHAR AS $$
 DECLARE
     signo_zodiacal VARCHAR;
 BEGIN
 
-    IF (EXTRACT(MONTH FROM data_nascimento) = 1 AND EXTRACT(DAY FROM data_nascimento) >= 20) OR (EXTRACT(MONTH FROM data_nascimento) = 2 AND EXTRACT(DAY FROM data_nascimento) <= 18) THEN
-        signo_zodiacal := 'Aquário';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 2 AND EXTRACT(DAY FROM data_nascimento) >= 19) OR (EXTRACT(MONTH FROM data_nascimento) = 3 AND EXTRACT(DAY FROM data_nascimento) <= 20) THEN
-        signo_zodiacal := 'Peixes';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 3 AND EXTRACT(DAY FROM data_nascimento) >= 21) OR (EXTRACT(MONTH FROM data_nascimento) = 4 AND EXTRACT(DAY FROM data_nascimento) <= 19) THEN
-        signo_zodiacal := 'Áries';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 4 AND EXTRACT(DAY FROM data_nascimento) >= 20) OR (EXTRACT(MONTH FROM data_nascimento) = 5 AND EXTRACT(DAY FROM data_nascimento) <= 20) THEN
-        signo_zodiacal := 'Touro';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 5 AND EXTRACT(DAY FROM data_nascimento) >= 21) OR (EXTRACT(MONTH FROM data_nascimento) = 6 AND EXTRACT(DAY FROM data_nascimento) <= 20) THEN
-        signo_zodiacal := 'Gêmeos';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 6 AND EXTRACT(DAY FROM data_nascimento) >= 21) OR (EXTRACT(MONTH FROM data_nascimento) = 7 AND EXTRACT(DAY FROM data_nascimento) <= 22) THEN
-        signo_zodiacal := 'Câncer';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 7 AND EXTRACT(DAY FROM data_nascimento) >= 23) OR (EXTRACT(MONTH FROM data_nascimento) = 8 AND EXTRACT(DAY FROM data_nascimento) <= 22) THEN
-        signo_zodiacal := 'Leão';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 8 AND EXTRACT(DAY FROM data_nascimento) >= 23) OR (EXTRACT(MONTH FROM data_nascimento) = 9 AND EXTRACT(DAY FROM data_nascimento) <= 22) THEN
-        signo_zodiacal := 'Virgem';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 9 AND EXTRACT(DAY FROM data_nascimento) >= 23) OR (EXTRACT(MONTH FROM data_nascimento) = 10 AND EXTRACT(DAY FROM data_nascimento) <= 22) THEN
+    IF (EXTRACT(MONTH FROM birthdate) = 1 AND EXTRACT(DAY FROM birthdate) >= 20) OR (EXTRACT(MONTH FROM birthdate) = 2 AND EXTRACT(DAY FROM birthdate) <= 18) THEN
+        signo_zodiacal := 'Aquarius';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 2 AND EXTRACT(DAY FROM birthdate) >= 19) OR (EXTRACT(MONTH FROM birthdate) = 3 AND EXTRACT(DAY FROM birthdate) <= 20) THEN
+        signo_zodiacal := 'Pisces';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 3 AND EXTRACT(DAY FROM birthdate) >= 21) OR (EXTRACT(MONTH FROM birthdate) = 4 AND EXTRACT(DAY FROM birthdate) <= 19) THEN
+        signo_zodiacal := 'Aries';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 4 AND EXTRACT(DAY FROM birthdate) >= 20) OR (EXTRACT(MONTH FROM birthdate) = 5 AND EXTRACT(DAY FROM birthdate) <= 20) THEN
+        signo_zodiacal := 'Taurus';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 5 AND EXTRACT(DAY FROM birthdate) >= 21) OR (EXTRACT(MONTH FROM birthdate) = 6 AND EXTRACT(DAY FROM birthdate) <= 20) THEN
+        signo_zodiacal := 'Gemini';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 6 AND EXTRACT(DAY FROM birthdate) >= 21) OR (EXTRACT(MONTH FROM birthdate) = 7 AND EXTRACT(DAY FROM birthdate) <= 22) THEN
+        signo_zodiacal := 'Cancer';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 7 AND EXTRACT(DAY FROM birthdate) >= 23) OR (EXTRACT(MONTH FROM birthdate) = 8 AND EXTRACT(DAY FROM birthdate) <= 22) THEN
+        signo_zodiacal := 'Leo';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 8 AND EXTRACT(DAY FROM birthdate) >= 23) OR (EXTRACT(MONTH FROM birthdate) = 9 AND EXTRACT(DAY FROM birthdate) <= 22) THEN
+        signo_zodiacal := 'Virgo';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 9 AND EXTRACT(DAY FROM birthdate) >= 23) OR (EXTRACT(MONTH FROM birthdate) = 10 AND EXTRACT(DAY FROM birthdate) <= 22) THEN
         signo_zodiacal := 'Libra';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 10 AND EXTRACT(DAY FROM data_nascimento) >= 23) OR (EXTRACT(MONTH FROM data_nascimento) = 11 AND EXTRACT(DAY FROM data_nascimento) <= 21) THEN
-        signo_zodiacal := 'Escorpião';
-    ELSIF (EXTRACT(MONTH FROM data_nascimento) = 11 AND EXTRACT(DAY FROM data_nascimento) >= 22) OR (EXTRACT(MONTH FROM data_nascimento) = 12 AND EXTRACT(DAY FROM data_nascimento) <= 21) THEN
-        signo_zodiacal := 'Sagitário';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 10 AND EXTRACT(DAY FROM birthdate) >= 23) OR (EXTRACT(MONTH FROM birthdate) = 11 AND EXTRACT(DAY FROM birthdate) <= 21) THEN
+        signo_zodiacal := 'Scorpio';
+    ELSIF (EXTRACT(MONTH FROM birthdate) = 11 AND EXTRACT(DAY FROM birthdate) >= 22) OR (EXTRACT(MONTH FROM birthdate) = 12 AND EXTRACT(DAY FROM birthdate) <= 21) THEN
+        signo_zodiacal := 'Sagittarius';
     ELSE
-        signo_zodiacal := 'Capricórnio';
+        signo_zodiacal := 'Capricorn';
     END IF;
 
     RETURN signo_zodiacal;
@@ -56,14 +56,14 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-INSERT INTO usuarios (nome, sobrenome, email, data_nascimento, idade, signo_zodiacal)
+INSERT INTO usuários (first_name, last_name, email, birthdate, age, signo_zodiacal)
 VALUES (
   'João1', 
   'Silva1', 
   'joao.silv1a@example.com', 
   '1990-09-15',
-  calcular_idade('1990-09-15'), 
-  calcular_signo_zodiacal('1990-09-15')
+  calculate_age('1990-09-15'), 
+  calculate_signo_zodiacal('1990-09-15')
 );
 
-SELECT * FROM usuarios;
+SELECT * FROM usuários;
